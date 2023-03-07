cf53eabebac0e2424bbf4a6e1f9c501d51e6703d
Munajat <bymunajat@gmail.com>
2023-03-07 22:15:05 +0700

    first commit

---
+++ Server/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+using FormulaOne.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaOne.Server.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options)
+    : base(options)
+    {
+
+    }
+    public DbSet<Driver> Drivers { get; set; }
+}
