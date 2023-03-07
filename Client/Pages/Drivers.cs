6fc5eadeda3d8965ce163de083fc5eab9ab830e5
Munajat <bymunajat@gmail.com>
2023-03-08 00:19:25 +0700

    update drivers

--- Client/Pages/Drivers.cs
+++ Client/Pages/Drivers.cs
@@ -1,6 +1,19 @@
+using FormulaOne.Client.Services;
+using FormulaOne.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
 namespace FormulaOne.Client.Pages;
 
 public partial class Drivers
 {
+    [Inject]
+    private IDriverService driverService { get; set; }
+    public IEnumerable<Driver> _drivers { get; set; } = new List<Driver>();
 
+    protected async override Task OnInitializedAsync()
+    {
+        var apiDrivers = await driverService.All();
+        if (apiDrivers != null && apiDrivers.Any())
+            _drivers = apiDrivers;
+    }
 }
